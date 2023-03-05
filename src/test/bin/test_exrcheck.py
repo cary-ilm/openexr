f7b61813fec07348a21461ef9ba776d359803277
Cary Phillips <cary@ilm.com>
2023-03-05 13:09:57 -0800

    test_exrcheck.py
    Signed-off-by: Cary Phillips <cary@ilm.com>

---
+++ src/test/bin/test_exrcheck.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sys, os
+from subprocess import PIPE, run
+
+print(f"testing exrcheck in python: {sys.argv}")
+
+bin_dir = sys.argv[1]
+src_dir = sys.argv[2]
+exr_file = sys.argv[3]
+
+exrinfo = f"{bin_dir}/exrinfo"
+exrcheck = f"{bin_dir}/exrcheck"
+
+result = run (["file", exr_file], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+print(f"file {' '.join(result.args[1:])}")
+print(result.stdout)
+
+result = run ([exrcheck, exr_file], stdout=PIPE, stderr=PIPE, universal_newlines=True)
+print(f"exrcheck {' '.join(result.args[1:])}")
+print(result.stdout)
+
+print("success.")
