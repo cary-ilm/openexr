c28900437740b1b7c385e12103c9b80bbe91b32f
Cary Phillips <cary@ilm.com>
2023-04-03 17:11:20 -0700

    libs first
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- share/ci/scripts/linux/yum/install_docs_env.sh
+++ share/ci/scripts/linux/yum/install_docs_env.sh
@@ -11,11 +11,12 @@ pip3 install -r $HERE/../../../../../docs/requirements.txt
 
 yum -y install wget
 
-wget https://imagemagick.org/archive/linux/CentOS/x86_64/ImageMagick-6.9.12-84.x86_64.rpm
-rpm -Uvh ImageMagick-6.9.12-84.x86_64.rpm
 
 wget https://imagemagick.org/archive/linux/CentOS/x86_64/ImageMagick-libs-7.1.1-6.x86_64.rpm
 rpm -Uvh ImageMagick-libs-7.1.1-6.x86_64.rpm
+
+wget https://imagemagick.org/archive/linux/CentOS/x86_64/ImageMagick-6.9.12-84.x86_64.rpm
+rpm -Uvh ImageMagick-6.9.12-84.x86_64.rpm
 
 which convert
 
