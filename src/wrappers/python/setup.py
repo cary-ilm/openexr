3303abcd6a91efee53e0bbf76234a720831382e2
Cary Phillips <cary@ilm.com>
2023-07-31 17:38:18 -0700

    remove z
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/wrappers/python/setup.py
+++ src/wrappers/python/setup.py
@@ -29,19 +29,15 @@ version_major, version_minor, version_patch = version
 version = f"{version_major}.{version_minor}.{version_patch}"
 
 libs=[]
-libs_static=['z',
-             f'OpenEXR-{version_major}_{version_minor}',
+libs_static=[f'OpenEXR-{version_major}_{version_minor}',
              f'IlmThread-{version_major}_{version_minor}',
              f'Iex-{version_major}_{version_minor}',
              f'Imath-3_1',
-             f'OpenEXRCore-{version_major}_{version_minor}',
-             f'deflate']
+             f'OpenEXRCore-{version_major}_{version_minor}']
 definitions = [('PYOPENEXR_VERSION_MAJOR', f'{version_major}'),
                ('PYOPENEXR_VERSION_MINOR', f'{version_minor}'),
                ('PYOPENEXR_VERSION_PATCH', f'{version_patch}'),]
 if platform.system() == "Windows":
-    libs_static[0] = 'zlibstatic'
-    libs_static[-1] = 'deflatestatic'
     definitions = [('PYOPENEXR_VERSION', f'\\"{version}\\"')]
 extra_compile_args = []
 if platform.system() == 'Darwin':
