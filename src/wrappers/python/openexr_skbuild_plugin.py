65024281e10f8ac80cc2d6ed3c6d439bf4cc070c
Cary Phillips <cary@ilm.com>
2024-02-07 15:31:48 -0800

    test19
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/wrappers/python/openexr_skbuild_plugin.py
+++ src/wrappers/python/openexr_skbuild_plugin.py
@@ -80,7 +80,7 @@ def dynamic_metadata(
 
     if "OPENEXR_VERSION_RELEASE_TYPE" in os.environ:
         rt = os.environ["OPENEXR_VERSION_RELEASE_TYPE"]
-        version += rt.replace('.','').replace('-','')
+        version += rt.replace('.','_')
         
     print("openexr_skbuild_plugin: Computed version: {0}".format(version))
 
