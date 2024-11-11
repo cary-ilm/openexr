19b1ac6ed86050009a6954454bb5e64d7fbd6aaa
Cary Phillips <cary@ilm.com>
2024-11-11 14:37:47 -0800

    install
    Signed-off-by: Cary Phillips <cary@ilm.com>

---
+++ share/ci/scripts/install_help2man.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -ex
+
+HELP2MAN_VERSION="1.49.3"
+HELP2MAN_URL="https://ftp.gnu.org/gnu/help2man/help2man-$HELP2MAN_VERSION.tar.xz"
+HELP2MAN_DIR="help2man-$HELP2MAN_VERSION"
+
+if [[ $OSTYPE == *msys* ]]; then
+    SUDO=""
+    export PROGRAM_SUFFIX=.exe
+else
+    SUDO="sudo"
+    export PROGRAM_SUFFIX=""
+fi
+
+# Download help2man source code
+echo "Downloading help2man version $HELP2MAN_VERSION..."
+curl -O "$HELP2MAN_URL"
+
+# Extract the downloaded tarball. On ChangeLog is a symlink, which
+# Windows doesn't like, so exclude it.
+echo "Extracting help2man..."
+tar -xf "$HELP2MAN_DIR.tar.xz" --exclude="*/ChangeLog"
+
+# Navigate into the help2man source directory
+cd "$HELP2MAN_DIR"
+
+# Configure, build, and install help2man
+echo "Configuring help2man..."
+./configure 
+
+echo "Building help2man..."
+make
+
+echo "Installing help2man..."
+$SUDO make install 
+
+if [[ $OSTYPE == *msys* ]]; then
+  mv /usr/local/bin/help2man /usr/local/bin/help2man$PROGRAM_SUFFIX
+fi
+
+ls -l /usr/local/bin
+
+# Verify the installation
+/usr/local/bin/help2man$PROGRAM_SUFFIX --version 
+
+echo "help2man installed successfully!"
+
+
