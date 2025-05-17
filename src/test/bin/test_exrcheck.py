821c07a94b3ef0d5e4303ee6bdb16ea20419daa2
Cary Phillips <cary@ilm.com>
2025-05-17 15:46:06 -0700

    more informative output from test_exrcheck.py
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/test/bin/test_exrcheck.py
+++ src/test/bin/test_exrcheck.py
@@ -10,31 +10,27 @@ print(f"testing exrcheck: {' '.join(sys.argv)}")
 
 exrcheck = sys.argv[1]
 image_dir = sys.argv[2]
+image_files = sys.argv[3:]
 
-for exr_file in sys.argv[3:]:
+def run_exrcheck(cmd):
+    result = run (cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    cmd_string = " ".join(result.args)
+    print(cmd_string)
+    if result.returncode != 0:
+        print(f"error: {cmd_string} failed: returncode={result.returncode}")
+        print(f"stdout:\n{result.stdout}")
+        print(f"stderr:\n{result.stderr}")
+        sys.exit(1)
+    
+for exr_file in image_files:
 
     exr_path = f"{image_dir}/{exr_file}"
 
-    result = run ([exrcheck, exr_path], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    print(" ".join(result.args))
-    assert(result.returncode == 0), "\n"+result.stderr
-
-    result = run ([exrcheck, "-m", exr_path], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    print(" ".join(result.args))
-    assert(result.returncode == 0), "\n"+result.stderr
-
-    result = run ([exrcheck, "-t", exr_path], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    print(" ".join(result.args))
-    assert(result.returncode == 0), "\n"+result.stderr
-
-    result = run ([exrcheck, "-s", exr_path], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    print(" ".join(result.args))
-    assert(result.returncode == 0), "\n"+result.stderr
-
-    result = run ([exrcheck, "-c", exr_path], stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    print(" ".join(result.args))
-    assert(result.returncode == 0), "\n"+result.stderr
-
+    run_exrcheck([exrcheck, exr_path])
+    run_exrcheck([exrcheck, "-m", exr_path])
+    run_exrcheck([exrcheck, "-t", exr_path])
+    run_exrcheck([exrcheck, "-s", exr_path])
+    run_exrcheck([exrcheck, "-c", exr_path])
 
 print("success.")
 
