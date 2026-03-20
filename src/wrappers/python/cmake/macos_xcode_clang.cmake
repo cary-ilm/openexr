f8aacf7a473cda6c3c6f71226d07ed1a40df0fa2
Cary Phillips <cary@ilm.com>
2026-03-19 17:23:30 -0700

    10.15
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- src/wrappers/python/cmake/macos_xcode_clang.cmake
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) Contributors to the OpenEXR Project.
-#
-# Used only for cibuildwheel macOS wheels. GitHub runners often put Homebrew
-# LLVM ahead of Xcode on PATH; linking that LLVM pulls in libunwind (min
-# macOS 14), which delocate then bundles and rejects vs older wheel tags.
-
-set (CMAKE_C_COMPILER "/usr/bin/clang" CACHE FILEPATH "" FORCE)
-set (CMAKE_CXX_COMPILER "/usr/bin/clang++" CACHE FILEPATH "" FORCE)
